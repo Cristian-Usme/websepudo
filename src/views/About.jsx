d0a804a6debd6a4116639be5c1d6f497604034d7
OscarRamos <oscara22a.r@gmail.com>
2024-06-13 15:58:54 -0500

    Avance en el diseo de eventos y nosotros

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,9 +1,18 @@
 // import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from '../../public/styles/About.module.css';
+import Slider from '../components/Slider';
 
 function About() {
     const navigate = useNavigate();
+
+    const slides = [
+        { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ2DpxEqyXpwMxLSlx1MVjuAQxoB58yOg-WA&s", title: "1" },
+        { url: "https://www.semana.com/resizer/v2/3UI6J6DICVBUVBCRPOAT3IUEKY.jpg?auth=67839dd55ebe7ca8f92ca63dcbb8a866764a375b1ae9ea0515d00980f4536437&smart=true&quality=75&width=1280&fitfill=false", title: "2" },
+        { url: "https://empresa.sumatec.co/wp-content/uploads/2019/10/portada-logistica-y-cadena-de-suministros-4-04.jpg", title: "3" },
+        { url: "https://img.freepik.com/foto-gratis/hombres-casco-trabajando-logistico_23-2148886815.jpg", title: "4" }
+    ];
+
     return (
         <div id={styles.body}>
             <div className={styles.contenido}>
@@ -15,6 +24,12 @@ function About() {
                 </div>
                 <img className={styles.imagen} src="https://cdnwordpresstest-f0ekdgevcngegudb.z01.azurefd.net/es/wp-content/uploads/2023/11/concierto-karol-g.jpg" alt="Imagen de prueba" />
             </div>
+            <section className={styles.sectionSlider}>
+                <h1 className={styles.title}>THE TEAMGAYS</h1>
+                <div className={styles.slider}>
+                    <Slider slides={slides} />
+                </div>
+            </section>
             <div className={styles.contenidoDos}>
                 <section>
                     <button onClick={() => navigate('/Events')} className={styles.borderButton}>
--- src/views/Events.jsx
+++ src/views/Events.jsx
@@ -1,90 +1,55 @@
 import styles from '../../public/styles/Events.module.css';
-import Slider from '../components/Slider';
 import Carousel from '../components/Carousel';
 
 function Events() {
     const eventSlides = [
         {
+            id: 1,
             artist: "BAD BUNNY",
             city: "Medellín",
+            description: "Concierto en el estadio Atanasio Girardot",
             img: "https://img.freepik.com/foto-gratis/luz-brillante-escenario-ilumina-entusiastas-fanaticos-rock-generados-ia_188544-37983.jpg?size=626&ext=jpg&ga=GA1.1.1687694167.1714348800&semt=sph"
         },
         {
+            id: 2,
             artist: "KAROL G",
             city: "Bogotá",
+            description: "Presentación en el parque Simón Bolívar",
             img: "https://www.bbva.com/wp-content/uploads/2020/05/festival2.jpg"
         },
         {
+            id: 3,
             artist: "DIOMEDES",
             city: "Ibiza",
-            img: "https://cc-prod.scene7.com/is/image/CCProdAuthor/concert-photography_P1_900x420?$pjpeg$&jpegSize=200&wid=900"
+            description: "Evento en la famosa discoteca Pacha",
+            img: "https://redmas.com.co/__export/1712706938307/sites/redmas/img/2024/04/09/feid-nuevas-fechas-concierto.jpg_1841751969.jpg"
         },
         {
+            id: 4,
             artist: "PESO PLUMA",
             city: "Madrid",
+            description: "Concierto en el WiZink Center",
             img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3Ud7B6vVH31RZ_aC1XvR7brvkYrVfvdFV1MHVuV-kYHmlxLosydaVznh0J9QQj8OpWMw&usqp=CAU"
         }
     ];
-    const slides = [
-        {url: "https://img.freepik.com/foto-gratis/luz-brillante-escenario-ilumina-entusiastas-fanaticos-rock-generados-ia_188544-37983.jpg?size=626&ext=jpg&ga=GA1.1.1687694167.1714348800&semt=sph", title: "1"},
-        {url: "https://www.bbva.com/wp-content/uploads/2020/05/festival2.jpg", title: "2"},
-        {url: "https://cc-prod.scene7.com/is/image/CCProdAuthor/concert-photography_P1_900x420?$pjpeg$&jpegSize=200&wid=900", title: "3"},
-        {url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3Ud7B6vVH31RZ_aC1XvR7brvkYrVfvdFV1MHVuV-kYHmlxLosydaVznh0J9QQj8OpWMw&usqp=CAU", title: "4"}
-    ];
+
+
+
     return (
         <div id={styles.body}>
             <h1 className={styles.title}>NUESTROS EVENTOS</h1>
-            <Carousel slides={eventSlides}/>
+            <Carousel slides={eventSlides} />
             <div className={styles.container}>
-            
-                <div className={styles.row}>
-                    <section className={styles.thumbnails}>
-                        <img className={styles.image} alt="1" src="https://img.freepik.com/foto-gratis/luz-brillante-escenario-ilumina-entusiastas-fanaticos-rock-generados-ia_188544-37983.jpg?size=626&ext=jpg&ga=GA1.1.1687694167.1714348800&semt=sph"/>
-                        <h2 className={styles.title}>KAROL G</h2>
-                        <h3 className={styles.city}>Medellín</h3>
-                    </section>
-                    <section className={styles.thumbnails}>
-                        <img className={styles.image} alt="2" src="https://www.bbva.com/wp-content/uploads/2020/05/festival2.jpg"/>
-                        <h2 className={styles.title}>BAD BUNNY</h2>
-                        <h3 className={styles.city}>Bogotá</h3>
-                    </section>
-                    <section className={styles.thumbnails}>
-                        <img className={styles.image} alt="3" src="https://cc-prod.scene7.com/is/image/CCProdAuthor/concert-photography_P1_900x420?$pjpeg$&jpegSize=200&wid=900"/>
-                        <h2 className={styles.title}>DIOMEDES</h2>
-                        <h3 className={styles.city}>Ibiza</h3>
-                    </section>
-                    <section className={styles.thumbnails}>
-                        <img className={styles.image} alt="4" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3Ud7B6vVH31RZ_aC1XvR7brvkYrVfvdFV1MHVuV-kYHmlxLosydaVznh0J9QQj8OpWMw&usqp=CAU"/>
-                        <h2 className={styles.title}>PESO PLUMA</h2>
-                        <h3 className={styles.city}>Buenos Aires</h3>
-                    </section>
-                </div>
-                <div className={styles.row}>
-                    <section className={styles.thumbnails}>
-                        <img className={styles.image} alt="1" src="https://img.freepik.com/foto-gratis/luz-brillante-escenario-ilumina-entusiastas-fanaticos-rock-generados-ia_188544-37983.jpg?size=626&ext=jpg&ga=GA1.1.1687694167.1714348800&semt=sph"/>
-                        <h2 className={styles.title}>KAROL G</h2>
-                        <h3 className={styles.city}>Medellín</h3>
-                    </section>
-                    <section className={styles.thumbnails}>
-                        <img className={styles.image} alt="2" src="https://www.bbva.com/wp-content/uploads/2020/05/festival2.jpg"/>
-                        <h2 className={styles.title}>KAROL G</h2>
-                        <h3 className={styles.city}>Medellín</h3>
-                    </section>
-                    <section className={styles.thumbnails}>
-                        <img className={styles.image} alt="3" src="https://cc-prod.scene7.com/is/image/CCProdAuthor/concert-photography_P1_900x420?$pjpeg$&jpegSize=200&wid=900"/>
-                        <h2 className={styles.title}>KAROL G</h2>
-                        <h3 className={styles.city}>Medellín</h3>
-                    </section>
-                    <section className={styles.thumbnails}>
-                        <img className={styles.image} alt="4" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3Ud7B6vVH31RZ_aC1XvR7brvkYrVfvdFV1MHVuV-kYHmlxLosydaVznh0J9QQj8OpWMw&usqp=CAU"/>
-                        <h2 className={styles.title}>KAROL G</h2>
-                        <h3 className={styles.city}>Medellín</h3>
-                    </section>
-                </div>
-            </div>
-            <h1 className={styles.title}>GALERÍA</h1>
-            <div className={styles.slider}>
-                <Slider slides={slides} />
+                {eventSlides.map(event => (
+                    <div key={event.id} className={styles.event}>
+                        <img className={styles.image} alt={event.artist} src={event.img} />
+                        <div className={styles.info}>
+                            <h2 className={styles.title}>{event.artist}</h2>
+                            <h3 className={styles.city}>{event.city}</h3>
+                            <p className={styles.description}>{event.description}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
