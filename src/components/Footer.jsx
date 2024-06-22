e2c83cec34c4d9ae4ad3ab0539bdde60dfb73e86
Cristian Usme <cristian1213u@gmail.com>
2024-06-21 22:44:33 -0500

    Actualizar Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,42 @@
-import "../../public/styles/Footer.css";
+import styles from '../../public/styles/Footer.module.css';
 
 const Footer = () => {
     return (
         <div>
-            <footer className="footer">
-                <div>
-                    <p>¡Gracias por visitar nuestro sitio!</p>
-                    <p>&copy; 2024 Mi Sitio Web</p>
-                    <p>Contacto: 555-555-5555</p>
-                    <p>Dirección: 123 Main St, Anytown, COL</p>
-                    <a href="https://www.instagram.com/" target="_blank"><i className="fa-brands fa-square-instagram"></i></a>
-                    <a href="https://www.linkedin.com/" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
-                    <a href="https://www.facebook.com/" target="_blank"><i className="fa-brands fa-square-facebook"></i></a>
-                    <a href="https://twitter.com/" target="_blank"><i className="fa-brands fa-square-x-twitter"></i></a>
-                    <a href="https://www.whatsapp.com/" target="_blank"><i className="fa-brands fa-square-whatsapp"></i></a>
+            <footer className={styles.footer}>
+                <div className={styles.linksSocial}>
+                    <img src="../../public/vite.svg" alt="" />
+                    <div>
+                        <h2>Enlaces</h2>
+                        <a className={styles.lblA} href="/">Inicio</a>
+                        <br />
+                        <a className={styles.lblA} href="/Contact">Contactanos</a>
+                        <br />
+                        <a className={styles.lblA} href="/JoinUs">Unete</a>
+                        <br />
+                        <a className={styles.lblA} href="/Events">Eventos</a>
+                    </div>
+                    <div>
+                        <h2>Contacto</h2>
+                        <p className={styles.lblP}>555-555-5555</p>
+                        <p className={styles.lblP}>Johndoe@doemail.com</p>
+                        <p className={styles.lblP}>123 Main St, Anytown, COL</p>
+                    </div>
+                    <div className={styles.socialNetworks}>
+                        <h2>Redes sociales</h2>
+                        <a className={styles.lblA} href="https://www.instagram.com/" target="_blank"><i className="fa-brands fa-square-instagram"></i><span>Instagram</span></a>
+                        <a className={styles.lblA} href="https://www.linkedin.com/" target="_blank"><i className="fa-brands fa-linkedin"></i><span>LinkedIn</span></a>
+                        <a className={styles.lblA} href="https://www.facebook.com/" target="_blank"><i className="fa-brands fa-square-facebook"></i><span>Facebook</span></a>
+                        <a className={styles.lblA} href="https://twitter.com/" target="_blank"><i className="fa-brands fa-square-x-twitter"></i><span>X</span></a>
+                        <a className={styles.lblA} href="https://www.whatsapp.com/" target="_blank"><i className="fa-brands fa-square-whatsapp"></i><span>WhatsApp</span></a>
+                    </div>
+                    <div>
+                        <h2>Equipo de desarrollo</h2>
+                        <img src="../../public/vite.svg" alt="" />
+                    </div>
                 </div>
+
+                <div className={styles.right}>&copy; Todos los derechos reservados 2024</div>
             </footer>
         </div>
     );
