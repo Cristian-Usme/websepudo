ad864d3c947e0e2a83fd01e4b55a89983ee9ec4d
OscarRamos <oscara22a.r@gmail.com>
2024-08-24 17:14:36 -0500

    Cambios en el contactanos

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -5,7 +5,6 @@ import TwitterIcon from '../../public/images/twitter.png';
 import FacebookIcon from '../../public/images/facebook.png';
 import YoutubeIcon from '../../public/images/youtube.png';
 import TelephoneIcon from '../../public/images/telephone.png';                      
-
 function Contact() {
     return (
         <div id={styles.body}>
@@ -13,29 +12,54 @@ function Contact() {
             <div className={styles.container}>
                 <section className={styles.contactItems}>
                     <div className={styles.channels}>
-                        <div className={styles.row}>
-                            <img src={InstagramIcon} alt="logo" className={styles.icon} />
-                            <h1>Instagram</h1>
+                        <div className={styles.socialMedia}>
+                            <h2 className={styles.sectionTitle}>Síguenos en Redes Sociales</h2>
+                            <p className={styles.sectionDescription}>
+                                Conéctate con nosotros a través de nuestras redes sociales para estar al día con las últimas noticias y actualizaciones.
+                            </p>
+                            <div className={styles.row}>
+                                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                                    <img src={InstagramIcon} alt="Instagram" className={styles.icon} />
+                                    <p className={styles.linkText}>Instagram</p>
+                                </a>
+                            </div>
+                            <div className={styles.row}>
+                                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                                    <img src={TwitterIcon} alt="Twitter" className={styles.icon} />
+                                    <p className={styles.linkText}>Twitter</p>
+                                </a>
+                            </div>
+                            <div className={styles.row}>
+                                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className={styles.link}>
+                                    <img src={FacebookIcon} alt="Facebook" className={styles.icon} />
+                                    <p className={styles.linkText}>Facebook</p>
+                                </a>
+                            </div>
+                            <div className={styles.row}>
+                                <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+                                    <img src={YoutubeIcon} alt="YouTube" className={styles.icon} />
+                                    <p className={styles.linkText}>YouTube</p>
+                                </a>
+                            </div>
                         </div>
-                        <div className={styles.row}>
-                            <img src={WhatsappIcon} alt="logo" className={styles.icon} />
-                            <h1>WhatsApp</h1>
-                        </div>
-                        <div className={styles.row}>
-                            <img src={TwitterIcon} alt="logo" className={styles.icon} />
-                            <h1>Twitter</h1>
-                        </div>
-                        <div className={styles.row}>
-                            <img src={FacebookIcon} alt="logo" className={styles.icon} />
-                            <h1>Facebook</h1>
-                        </div>
-                        <div className={styles.row}>
-                            <img src={YoutubeIcon} alt="logo" className={styles.icon} />
-                            <h1>YouTube</h1>
-                        </div>
-                        <div className={styles.row}>
-                            <img src={TelephoneIcon} alt="logo" className={styles.icon} />
-                            <h1>Línea de atención</h1>
+
+                        <div className={styles.contactMethods}>
+                            <h2 className={styles.sectionTitle}>Medios de Contacto</h2>
+                            <p className={styles.sectionDescription}>
+                                Contáctanos directamente a través de nuestros canales de comunicación para recibir asistencia personalizada.
+                            </p>
+                            <div className={styles.row}>
+                                <a href="tel:+1234567890">
+                                    <img src={TelephoneIcon} alt="Línea de atención" className={styles.icon} />
+                                    <p className={styles.linkText}>Línea de atención</p>
+                                </a>
+                            </div>
+                            <div className={styles.row}>
+                                <a href="https://wa.me/1234567890" target="_blank" rel="noopener noreferrer">
+                                    <img src={WhatsappIcon} alt="WhatsApp" className={styles.icon} />
+                                    <p className={styles.linkText}>WhatsApp</p>
+                                </a>
+                            </div>
                         </div>
                     </div>
                     <div>
