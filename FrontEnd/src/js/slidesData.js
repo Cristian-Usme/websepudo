2038751d64032eab23d12cb63eb2af32146d5866
srueda99 <ruedamarinsebastian@gmail.com>
2025-03-18 11:13:53 -0500

    Cambiando los textos

--- FrontEnd/src/js/slidesData.js
+++ FrontEnd/src/js/slidesData.js
@@ -9,35 +9,39 @@ export const dataSlide = [
 export const eventSlides = [
     {
         id: 1,
-        conciert: "NADIE SABE LO QUE VA A PASAR",
+        concert: "NADIE SABE LO QUE VA A PASAR",
         artist: "BAD BUNNY",
         city: "Medellín",
-        description: "Concierto en el estadio Atanasio Girardot jdkaskdkasdjksdhjfbafbyasbdhabsdchbsahcdbahsjbcasbdchjbasdhcjbadsljcblhsadbcahsbdclcblhbaschbasdchjdbdlahcbhasbclhbaslcbhdasbclhbashdcbalhbchlbhsbadchjbasdhvbhdsbvhjcxbvhjbvbdvhbhcbvhjzvbhjbzvhzhjxvblbhjzxvbzlbvhjlzbxvhlzbvhcxnasjdhjkasdjkasdjkbashbchjasbchjshjcbhjxzcbhjzbxhjc bczxhjc bhjz bhjzx hbzxhj bxhj bhjzb hjxzchjbxzhjcbshjabdfhcsdfhusebfhbsdhgfcsdbc sdsufgseuf hshfusgfusfh syuf sudfh sfyusgfusdf",
-        img: "https://img.freepik.com/foto-gratis/luz-brillante-escenario-ilumina-entusiastas-fanaticos-rock-generados-ia_188544-37983.jpg?size=626&ext=jpg&ga=GA1.1.1687694167.1714348800&semt=sph"
+        description: "Concierto en el estadio Atanasio Girardot, Medellín. Este evento contará con la participación de varios artistas invitados \
+        y una producción espectacular. La fecha del evento es el 15 de diciembre de 2023. Las entradas están a la venta en línea y en puntos de venta autorizados.",
+        img: "https://media.pitchfork.com/photos/65242c9caeb62be7989dbc83/1:1/w_3000,h_3000,c_limit/Bad-Bunny.jpg"
     },
     {
         id: 2,
-        conciert: "MAÑANA SERÁ MAS BONITO",
+        concert: "MAÑANA SERÁ MAS BONITO",
         artist: "KAROL G",
         city: "Bogotá",
-        description: "Presentación en el parque Simón Bolívar ghghggffggfgvjkhunjk  jipoijknuyftrdvbjopiuyfghv bjkliouythgfvnbhjkuiyuhgbnmjkjnbmjkhjbnm,jklhjbmn",
-        img: "https://www.bbva.com/wp-content/uploads/2020/05/festival2.jpg"
+        description: "Concierto en el estadio Nemesio Camacho el Campín, Bogotá. Este evento contará con la participación de varios artistas invitados \
+        y una producción espectacular. La fecha del evento es el 15 de diciembre de 2023. Las entradas están a la venta en línea y en puntos de venta autorizados.",
+        img: "https://www.soldierfield.com/assets/img/Static_Homepage_1920x1080_KarolG_2023_National-544b1e0bde.jpg"
     },
     {
         id: 3,
-        conciert: "FERXXO CALIPSIS",
-        artist: "DIOMEDES",
-        city: "Ibiza",
-        description: "Evento en la famosa discoteca Pacha jdjashdjkashdkjhasjdha jskhdkashdjkashjdhasudhjakshafyuehf adsjfhdasjkfhuiewhfuiahdsjk",
-        img: "https://redmas.com.co/__export/1712706938307/sites/redmas/img/2024/04/09/feid-nuevas-fechas-concierto.jpg_1841751969.jpg"
+        concert: "FERXXO CALIPSIS",
+        artist: "FEID",
+        city: "Medellín",
+        description: "Concierto en el estadio Atanasio Girardot, Medellín. Este evento contará con la participación de varios artistas invitados \
+        y una producción espectacular. La fecha del evento es el 07 de diciembre de 2024. Las entradas están a la venta en línea y en puntos de venta autorizados.",
+        img: "https://pechangaarenasd.com/wp-content/uploads/Static_Digital_500x250_FEID_2024_Regional_PechangaArena_0504.jpg"
     },
     {
         id: 4,
-        conciert: "PESO PLUMA",
-        artist: "PESO PLUMA",
-        city: "Madrid",
-        description: "Concierto en el WiZink Center",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3Ud7B6vVH31RZ_aC1XvR7brvkYrVfvdFV1MHVuV-kYHmlxLosydaVznh0J9QQj8OpWMw&usqp=CAU"
+        concert: "LAS MUJERES YA NO LLORAN TOUR",
+        artist: "Shakira",
+        city: "Barranquilla",
+        description: "Concierto en el estadio Metropolitano, Barranquilla. Este evento contará con la participación de varios artistas invitados \
+        y una producción espectacular. La fecha del evento es el 15 de diciembre de 2023. Las entradas están a la venta en línea y en puntos de venta autorizados.",
+        img: "https://mayacomunicacion.com.mx/wp-content/uploads/2022/09/shakira-2.jpg"
     }
 ];
 
--- FrontEnd/src/views/Events.jsx
+++ FrontEnd/src/views/Events.jsx
@@ -1,24 +1,31 @@
-import styles from '../../public/styles/Events.module.css';
-import Carousel from '../components/Carousel';
-import { eventSlides } from '../js/slidesData';
+import styles from "../../public/styles/Events.module.css";
+import Carousel from "../components/Carousel";
+import { eventSlides } from "../js/slidesData";
 
 function Events() {
-
     return (
         <div id={styles.body}>
             <h1 className={styles.title}>NUESTROS EVENTOS</h1>
             <Carousel slides={eventSlides} />
             <div className={styles.container}>
-                {eventSlides.map(event => (
+                {eventSlides.map((event) => (
                     <div key={event.id} className={styles.event}>
-                        <img className={styles.image} alt={event.artist} src={event.img} />
+                        <img
+                            className={styles.image}
+                            alt={event.artist}
+                            src={event.img}
+                        />
                         <div className={styles.info}>
-                            <h2 className={styles.title}>{event.conciert}</h2>
+                            <h2 className={styles.title}>{event.concert}</h2>
                             <section className={styles.nameAndCountry}>
-                                <h2 className={styles.artist}>{event.artist} en </h2>
+                                <h2 className={styles.artist}>
+                                    {event.artist} en{" "}
+                                </h2>
                                 <h3 className={styles.city}>{event.city}</h3>
                             </section>
-                            <p className={styles.description}>{event.description}</p>
+                            <p className={styles.description}>
+                                {event.description}
+                            </p>
                         </div>
                     </div>
                 ))}
